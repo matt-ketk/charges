c11d5a620879f0debc9748a3296f661e75675680
ATMallen8 <ATMallen8@gmail.com>
2019-11-25 22:23:14 +0000

    Added wire and modified charge

--- charge.py
+++ charge.py
@@ -1,6 +1,11 @@
 from constants import Constants
+import numpy as np
 
 class Charge:
-  def __init__(self, charge):
+  def __init__(self, charge, position = np.zeros(3), velocity = np.zeros(3)):
     self.charge = charge * Constants.E
     self.mass = Constants.MASS_ELECTRON
+    self.position = position
+    self.velocity = velocity
+
+  
