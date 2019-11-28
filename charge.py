ba23b90212d429a6e57cf2617ba7345806a1ebff
AlexTMallen <ATMallen8@gmail.com>
2019-11-28 02:44:55 +0000

    the checkCollision method of Wire has been completed and tested for edge cases such as infinite slopes and should be fully functional

--- charge.py
+++ charge.py
@@ -2,14 +2,6 @@ from constants import Constants
 import numpy as np
 
 class Charge:
-  massList = np.array()
-  posList = np.ndarray((, 3))
-  posXList = np.array()
-  posYList = np.array()
-  posZList = np.array()
-
-  chargeList = np.array()
-  
   def __init__(self, charge, position = np.zeros(3), velocity = np.zeros(3)):
     '''
     charge: charge of the particle in Coulombs
@@ -22,11 +14,6 @@ class Charge:
     self.prevPosition = np.zeros(3)
     self.velocity = velocity
 
-    # every time charge obj is init'ed, add the info to a class-wide list
-    np.stack()
-    np.append(self.posXList, self.position[0])
-    np.append(self.posYList, self.position[1])    np.append(self.posZList, self.position[2])
-    np.append(self.massList, self.mass)
 
 
     
