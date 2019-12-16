1aa25d37dbd62a3b3706696ff471fc678493b925
AlexTMallen <atmallen8@gmail.com>
2019-12-15 21:10:06 -0800

    Finished and tested collisions with wires not axis aligned

--- wire.py
+++ wire.py
@@ -11,7 +11,7 @@ class Wire(Conductor):
     def __init__(self, start, lengthV, r, resistivity=Constants.COPPER_RESISTIVITY):
         """
         start: length-3 numpy array representing the starting coordinates of the cylindrical wire
-        lengthV: length-3 numpy array (vector) that represents the length of the wire. 2 of the entries should be zero to make the cylinder orthogonal to the basis
+        lengthV: length-3 numpy array (vector) that represents the length of the wire.
         r: radius of wire
         """
         super(Wire, self).__init__(resistivity=resistivity)
@@ -48,72 +48,93 @@ class Wire(Conductor):
     def checkCollision(self, prevPos, pos, vel, dampeningFactor=1):
         """
         checks for collision of a particle with the cylinder given its position (as a numpy array) at 2 successive iterations
-        returns: the position of the collision and the particle's new velocity vector. If there was no colision returns None.
+        :param prevPos: particle's previous position
+        :param pos: particle's current position
+        :param vel: particle's velocity
+        :param dampeningFactor: factor by which the collision should slow down the velocity of the particle
+        :return: position of the collision and a 2D numpy array of particle's new velocity vector
+        returns None if no collision
         """
-        #TODO: make the input an array of positions/velocities and use numpy operations rather than a for loop
+        # checks for collision of a particle with the cylinder given its position (as a numpy array) at 2 successive iterations
+        # :param prevPos: 2D numpy array where each row represents a particle's previous position
+        # :param pos: 2D numpy array where each row represents a particle's current position
+        # :param vel: 2D numpy array where each row represents a particle's velocity
+        # :param dampeningFactor: factor by which the collisions should slow down the velocity of the particle
+        # :return: 2D numpy array of positions of the collision (pos[i] if no collision)
+        # and a 2D numpy array of each particle's new velocity vectors (vel[i] if no collision)
 
         # if the point didn't move ignore it
         if all(pos == prevPos):
             return None
 
-        # find orientation of wire
-        x, y, z = self.getXYZ()
+        basis = list()
+        # generate some random vector that is not in the same direction as lengthV
+        randMatrix = np.array([[1, 1, 0],
+                               [0, 1, 2],
+                               [-1, -1, 2]])
+        randVec = randMatrix @ self.lengthV
+        basis.append(np.cross(self.lengthV, randVec))
+        basis[0] /= np.linalg.norm(basis[0])
+        basis.append(np.cross(self.lengthV, basis[0]))
+        basis[1] /= np.linalg.norm(basis[1])
+        basis.append(self.lengthV / np.linalg.norm(self.lengthV))
+        basis = np.array(basis, dtype=float)
+        changeBasisM = basis.transpose()
+        invChangeBasisM = np.linalg.inv(changeBasisM)
 
+        nPrevPos = changeBasisM @ prevPos
+        nPos = changeBasisM @ pos
+        nStart = changeBasisM @ self.start
+        nEnd = changeBasisM @ self.end
+        nVel = changeBasisM @ vel
+
+        tempColPos = np.zeros(3)
         # ###---find x coordinate of collision (xCol) if its within the circle---###
-        m = (prevPos[y] - pos[y]) / (prevPos[x] - pos[x])
+        m = (nPrevPos[1] - nPos[1]) / (nPrevPos[0] - nPos[0])
         # calculate with x and y swapped if the slope is infinite so now the slope is 0
-        if np.isinf(m):
-            x, y = y, x
-            m = (prevPos[y] - pos[y]) / (prevPos[x] - pos[x])
 
-        A = -m * prevPos[x] + prevPos[y] - self.start[y]
-        mz = (prevPos[z] - pos[z]) / (prevPos[x] - pos[x])
+        a = -m * nPrevPos[0] + nPrevPos[1] - nStart[1]
+        mz = (nPrevPos[2] - nPos[2]) / (nPrevPos[0] - nPos[0])
         if np.isinf(mz):
             return None
 
-        xColTemp = Wire.quad(1 + m ** 2, 2 * (A * m + self.start[x]), A ** 2 + self.start[x] ** 2 - self.r ** 2)
+        xColTemp = Wire.quad(1 + m ** 2, 2 * (a * m + nStart[0]), a ** 2 + nStart[0] ** 2 - self.r ** 2)
 
         withinSegment = False
-        if len(xColTemp) == 0:
-            return None
-        if len(xColTemp) == 1:
+        if len(xColTemp) == 0 or len(xColTemp) == 1:
             return None
         else:
-            xColTemp = [xc for xc in xColTemp if Plate.inInterval(xc, (prevPos[x], pos[x]))]
+            xColTemp = [xc for xc in xColTemp if Plate.inInterval(xc, (nPrevPos[0], nPos[0]))]
             if len(xColTemp) != 0:
                 withinSegment = True
-                xCol = min(xColTemp, key=lambda p: abs(p - prevPos[0]))
+                tempColPos[0] = min(xColTemp, key=lambda p: abs(p - nPrevPos[0]))
 
         if not withinSegment:
             return None
 
         # find y coordinate of collision (yCol)
-        yCol = m * (xCol - prevPos[x]) + prevPos[y]
+        tempColPos[1] = m * (tempColPos[0] - nPrevPos[0]) + nPrevPos[1]
 
         # find z coordinate of collision (zCol) and check if it's within the length vector of the cylinder
-        zCol = mz * (xCol - prevPos[x]) + prevPos[z]
-        withinHeight = min(self.start[z], self.end[z]) <= zCol <= max(self.start[z], self.end[z])
-        
+        tempColPos[2] = mz * (tempColPos[0] - nPrevPos[0]) + nPrevPos[2]
+        withinHeight = min(nStart[2], nEnd[2]) <= tempColPos[2] <= max(nStart[2], nEnd[2])
 
         if not withinHeight:
             return None
 
-        colPos = np.zeros(3)
-        colPos[z] = zCol
-        colPos[x] = xCol
-        colPos[y] = yCol
-
         # find normal vector to the cylinder to calculate the new velocity
         normal = np.zeros(3)
-        normal[x] = xCol - self.start[x]
-        normal[y] = yCol - self.start[y]
-        normal[z] = 0
+        normal[0] = tempColPos[0] - nStart[0]
+        normal[1] = tempColPos[1] - nStart[1]
+        normal[2] = 0
         normal /= self.r
 
-        #print(vel, np.sum(normal ** 2 ))
+        tempVel = dampeningFactor * (nVel - 2 * normal * np.dot(normal, nVel))
 
-        newVel = dampeningFactor * (vel - 2 * normal * np.dot(normal, vel))
-        return colPos, newVel
+        outputVel = invChangeBasisM @ tempVel
+        colPos = invChangeBasisM @ tempColPos
+
+        return colPos, outputVel
     
     def compile(self, list):
         self.coords = []
@@ -196,6 +217,3 @@ class Wire(Conductor):
         
         points = [[l1[0], l1[1]], [l2[0], l2[1]]]
         environment.drawMeshRaw(points, color)
-
-
-    
